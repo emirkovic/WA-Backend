11413c951f8cc2b57e5f0c478c35750ea901e460
Erik <emirkovic@student.unipu.hr>
2023-04-21 07:16:13 +0200

    cloudinray fix

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,1 +1,10 @@
-module.exports = { cloudinary: require('cloudinary').v2.config({ cloud_name: process.env.CLOUDINARY_CLOUD_NAME, api_secret: process.env.CLOUDINARY_API_SECRET, api_key: process.env.CLOUDINARY_API_KEY }) };
+const cloudinary = require('cloudinary').v2;
+require("dotenv").config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_key: process.env.CLOUDINARY_API_KEY
+});
+
+module.exports = { cloudinary };
